dd68c07fd44d93e57389b8a3bfab56b495e96015
anurag <anuragmahadev2000@mail.com>
2022-03-23 15:42:01 +0530

    Updates

---
+++ precache-manifest.c77520a854139584c97fe6f0077b21b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdfcc5da79f5ff2a375287aeb895535",
+    "url": "/my-app1/index.html"
+  },
+  {
+    "revision": "14677089a0b60401ee0e",
+    "url": "/my-app1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dac2b166da06952a189b",
+    "url": "/my-app1/static/js/2.589c40aa.chunk.js"
+  },
+  {
+    "revision": "14677089a0b60401ee0e",
+    "url": "/my-app1/static/js/main.39dd9cb8.chunk.js"
+  },
+  {
+    "revision": "5062bc4521fc16c49ada",
+    "url": "/my-app1/static/js/runtime~main.9bda2ebc.js"
+  }
+]);
